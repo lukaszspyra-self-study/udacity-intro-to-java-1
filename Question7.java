24c0fc978b03bc7c44b01b2500f2fb2e0cd69673
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-02-08 20:13:52 +0100

    Answer to task7 added

---
+++ Question7.java
@@ -0,0 +1,11 @@
+public class Question7 {
+    
+    public static void main(String[] args) {
+        
+        double fahrenheit = 77.0;
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println(celsius);
+
+    }
+}
