3b1d468952c30dfb169fd407c9ce36cd5d63c369
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-02-08 20:07:50 +0100

    Answer to task6 added

---
+++ Question6.java
@@ -0,0 +1,12 @@
+public class Question6{
+
+	public static void main (String[] args){
+
+		private String firstName = "Lukasz";
+		private String lastName = "Kowalski";
+		private String fullNAme = firstName + " " + lastName;
+
+		System.out.println("Hello, my name is " + fullName + ".");
+		System.out.println("There are " + fullName.length() + "letters in my name.");
+	}
+}
